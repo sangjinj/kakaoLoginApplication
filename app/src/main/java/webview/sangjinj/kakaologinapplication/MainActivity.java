01f7803f7d25a5535b5616b2d1f56a22708a21a8
sangjin <sangjinj7@gmail.com>
2016-08-10 14:47:00 +0900

    Initial commit

---
+++ app/src/main/java/webview/sangjinj/kakaologinapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package webview.sangjinj.kakaologinapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
